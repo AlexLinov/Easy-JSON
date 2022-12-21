1455b285f437516b2bf57462708109bddee62408
actuaL aka Alex <74632540+actua-L@users.noreply.github.com>
2022-12-21 09:27:27 -0500

    Update Easy-JSON.ps1

--- Easy-JSON.ps1
+++ Easy-JSON.ps1
@@ -1,14 +1,15 @@
 [CmdletBinding()]
 param
     (
-        [parameter(Mandatory=$true)][String]$file
-        
+        [parameter(Mandatory=$true)][String]$file,
+		
+        [parameter(Mandatory=$true)][String]$output
     )
-
+    
 $json = Get-Content $file
 
 $data = ConvertFrom-Json $json
 
 $ip = $data.ipAddress #change to desired JSON object
 
-Write-Output $ip | Out-File file.txt
+Write-Output $ip | Out-File $output
